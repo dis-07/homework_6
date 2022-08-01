d6b98534c372052f81293ebdd89c3817ae7dcfbf
Vlad Yefremenko <vladyefremenko@Vlads-MacBook-Pro.local>
2022-08-01 20:18:14 +0300

    first code

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let name = prompt ('what is your name ?');
+
+alert (name);
+console.log(name);
+
+let a = 20;
+
+let b = 5;
+
+alert (a + b);
+alert (a - b);
+alert (a * b);
+alert (a / b);
+alert (a **b);
